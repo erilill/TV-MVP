b43daec680679a97ba4b7ae312f38a8318882a64
Yukai Yang <yukai.yang@gmail.com>
2025-02-25 09:26:56 +0100

    make the R6 type TVMVP class

--- R/R6class.R
+++ R/R6class.R
@@ -1,2 +1,26 @@
 ## In this file, we define the R6 type class of the package
 
+#' Time Varying Minimum Variance Portfolio (TVMVP) Class
+#'
+#' This class implements a time-varying mean-variance portfolio model.
+#'
+#' @export
+TVMVP <- R6::R6Class(
+  "TVMVP",
+  
+  public = list(
+    
+    initialize = function(data = NULL) {
+      self$data <- data
+    }
+    
+  ),
+  
+  private = list(
+    # put all the variables here for encapsulation
+    # and offer public functions for users to get access them
+    
+    data <- NULL,
+    
+  )
+)
