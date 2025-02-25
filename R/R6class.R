a410399a49b2a20f07971aabf26d2a1f80f83534
Yukai Yang <yukai.yang@gmail.com>
2025-02-25 09:34:38 +0100

    add information about the successful loading of the data

--- R/R6class.R
+++ R/R6class.R
@@ -11,8 +11,11 @@ TVMVP <- R6::R6Class(
   public = list(
     
     initialize = function(data = NULL) {
-      if(is_tibble(data)){
+      if(tibble::is_tibble(data)){
         self$data <- data
+        cli::cli_alert_success("Tibble data set successfully loaded.")
+        # can also give the information of the data set
+        # for example, the size in MB, row and column numbers etc.
       } esle{
       }
     }
