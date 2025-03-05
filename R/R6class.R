07e98cde2c99521c96f3ed5aeb3e13c6e21128ea
Yukai Yang <yukai.yang@gmail.com>
2025-03-05 17:45:28 +0100

    control the warning message if the data is empty.

--- R/R6class.R
+++ R/R6class.R
@@ -33,7 +33,7 @@ TVMVP <- R6::R6Class(
       # this is why I use missing(data) for checking without setting any default value of it
       # note that this is different from initialize function where data can be missing with default value NULL
       if(missing(data)){
-        cli::cli_alert_warning("You forgot input the data!")
+        cli::cli_alert_warning("The data is empty.")
       } else{
         private$data <- data
         private$iT <- nrow(data)
@@ -50,6 +50,10 @@ TVMVP <- R6::R6Class(
     # get functions
 
     get_data = function(){
+      if(is.null(private$data)){
+        cli::cli_alert_warning("The data is empty.")
+        return(invisible(NULL))
+      }
       return(private$data)
       # make a copy of the data and return it
     }
