64fcd5c808793d83d42afa9959c3bddd6d364e56
Erik Lillrank <erik.lillrank@gmail.com>
2025-03-05 22:31:12 +0100

    removed old comments

--- TVMVP-workflow-example.R
+++ TVMVP-workflow-example.R
@@ -3,12 +3,8 @@ library(TVMVP)
 
 # Things that I am unsure of/needs work:
 # - determine_factors, localPCA: slow
-# - cv_bandwidth: is this necessary? Might need to look at other articles.
 # - rolling_time_varying_mvp, predict_portfolio: I think it works
-# - compute_residual_covariance: Works but problems with singularity, probably due to some problem with local_pca
 # - local_pca: Worked a lot in order to get this to work properly, still not sure.
-# - Licence of quadprog and spcov: GPL 2/>=2. If I want to use them, do I also need to use GNU?
-# - Realised that it is not really necessary for the end user to to do the localPCA themselves. Made it more userfriendly.
 
 set.seed(123)
 T <- 100  # Number of time periods
@@ -36,11 +32,6 @@ mvp_result <- rolling_time_varying_mvp(
   kernel_func    = epanechnikov_kernel,
   rf             = NULL
 )
-## K: some problem with non-conformable arguments
-## K: Error in factors_t %*% t(loadings_t) : non-conformable arguments
-## E: When initial_window is small, and m is large, the effective rank in the cv_bandwidth becomes < m which causes problems
-## E: Tried to fix it. I am not sure if this is a valid solution to the problem.
-
 prediction <- predict_portfolio(returns, 
                                 horizon = 21, 
                                 max_factors = 10,
