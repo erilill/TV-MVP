93c947cd6e855b53cb9f5f7d0e75d70cbf8ebdea
Erik Lillrank <erik.lillrank@gmail.com>
2025-05-15 15:12:22 +0200

    Updated package description

--- R/TVMVP.R
+++ R/TVMVP.R
@@ -18,6 +18,7 @@
 #'
 #' An optional step includes a hypothesis test to check whether the covariance 
 #' matrix is time-invariant.
+#' 
 #'
 #' @section Authors and Maintainer:
 #' Authors: Erik Lillrank and Yukai Yang \cr
@@ -29,10 +30,12 @@
 #' \describe{
 #'   \item{\code{\link{determine_factors}}}{Selects the optimal number of factors via an information criterion.}
 #'   \item{\code{\link{hyptest1}}}{Hypothesis test for time-invariant covariance matrices. Bootstrap p-values supported.}
+#'   \item{\code{\link{predict_portfolio}}}{Optimizes portfolio weights for out-of-sample prediction of portfolio performance.}
 #'   \item{\code{\link{rolling_time_varying_mvp}}}{Evaluates MVP performance in a rolling window framework.}
-#'   \item{\code{\link{predict_portfolio}}}{Optimizes portfolio weights out-of-sample.}
 #'   \item{\code{\link{time_varying_cov}}}{Estimates the time-varying covariance matrix.}
+#'   \item{\code{\link{silverman}}}{Silverman's rule of thumb bandwidth formula.}
 #' }
+#' 
 #' 
 #' @section Class:
 #' \describe{
@@ -40,6 +43,17 @@
 #' } 
 #' 
 #'
+#' @details
+#' The default kernel function in the package is the Epanechnikov kernel. Other
+#' kernel functions can also be used, however these are not implemented in the
+#' package. In order to do this, write an R function with an integrable
+#' kernel function and use this as input in the functions with argument
+#' \code{kernel_func}. It should be constructed as \code{custom_kernel <- function(u){...}}.
+#' 
+#' Similarly, the bandwidth function which is implemented in the package is 
+#' the Silverman's rule of thumb. For most functions, simply set \code{bandwidth}
+#' to your preferred bandwidth, however for \code{\link{rolling_time_varying_mvp}},
+#' only Silverman's is implemented in this version of the package.
 #' 
 #' @section References: 
 #' Lillank, E. (2025). A Time-Varying Factor Approach to Covariance Estimation.
